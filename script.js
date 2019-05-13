230949466f1041df381de41449495ac91715dd83
Kamandula, Akash Prasad <kamandap@miamioh.edu>
2019-05-13 18:14:27 -0400

    intainlizing the contents and working in progress

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 $(function() {
-    $('#mazeForm').on('submit', function(event) {
-        var answer = 'Word: "", Score: '; 
-        $('#mazeSolution').html(answer);
+    let maxWord = "";
+    let maxScore = 0;
+    let direction = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    let vowels = ["a", "e", "i", "o", "u"];
+
+    function getScore(c) {
+        if (c === "y") {
+            return -10;
+        }
+
+        for (let vowel in vowels) {
+            if (c === vowel) {
+                return 3;
+            }
+        }
+
+        return -2;
+    }
+
+    $("#mazeForm").on("submit", function(event) {
+        let mazeEntry = $.trim($("#mazeEntry").val());
+        let lines = mazeEntry.split("\n");
+
+        console.log('score:' + getScore('a'));
+
+        var answer = 'Word: "", Score: ';
+        $("#mazeSolution").html(mazeEntry);
         event.preventDefault();
     });
 });
